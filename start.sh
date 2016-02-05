a50bc3e12c99f4c65c342f05722ad77cc3aac4cf
uevol <uevol@outlook.com>
2016-02-05 16:09:29 +0800

    change the path in start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #start  dht22
-cd /home/pi/dht22
+cd /home/pi/raspberrypi/dht22
 sudo ruby dht22.rb &
 
 #start web service
-cd /home/pi/temphumi
+cd /home/pi/raspberrypi/temphumi
 rails s &
